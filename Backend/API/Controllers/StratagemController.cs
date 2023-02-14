86a99d9cec7d0cf7761baa549be6f5de5c50e16d
PhilEZad <90150224+PhilEZad@users.noreply.github.com>
2023-02-14 23:58:28 +0100

    Added http endpoint

---
+++ Backend/API/Controllers/StratagemController.cs
@@ -0,0 +1,21 @@
+﻿using Application;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class StratagemController
+{
+
+    public StratagemController()
+    {
+        
+    }
+    
+    [HttpGet]
+    public void GetAllStratagems()
+    {
+        
+    }   
+}
