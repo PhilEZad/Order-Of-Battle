baaecb07f5d69790346b324254c0586eb1fe8975
PhilEZad <90150224+PhilEZad@users.noreply.github.com>
2023-02-14 23:46:23 +0100

    Added missing methods

--- Backend/Application/Interfaces/IFactionService.cs
+++ Backend/Application/Interfaces/IFactionService.cs
@@ -5,7 +5,11 @@ namespace Application.Interfaces;
 
 public interface IFactionService
 {
+    public Faction CreateFaction(FactionRequest faction);
+    
     public Faction ReadFaction(int id);
 
     public List<Faction> GetAllFactions();
+    
+    public Faction UpdateFaction(Faction faction);
 }
