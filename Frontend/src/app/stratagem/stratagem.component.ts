19ff9e29b72fb8a0c6d7ae907edbca0e97841867
PhilEZad <90150224+PhilEZad@users.noreply.github.com>
2023-02-16 23:36:43 +0100

    More changes to CSS

--- Frontend/src/app/stratagem/stratagem.component.ts
+++ Frontend/src/app/stratagem/stratagem.component.ts
@@ -9,13 +9,19 @@ export class StratagemComponent implements OnInit {
 
   title = 'BREACH AND CLEAR'
   subtitle = 'T’au Empire – Battle Tactic Stratagem'
-  cp = '1/2'
+  cp = '1CP/2CP'
   flavourText = 'Breacher Teams excel at clearing enemy-held structures with blinding photon grenades and disciplined bursts of pulse fire.'
-  description = 'Use this Stratagem in your Shooting phase, when a BREACHER TEAM unit from your army is selected to shoot. Until the end of the phase, each time a CORE model in that unit makes a ranged attack:'
+  descriptionTop = 'Use this Stratagem in your Shooting phase, when a BREACHER TEAM unit from your army is selected to shoot. Until the end of the phase, each time a CORE model in that unit makes a ranged attack:'
+  descriptionBottom = ''
   list: any[] = ['The target does not receive the benefits of cover against that attack.', 'You can re-roll the wound roll.'];
   constructor() { }
 
   ngOnInit(): void {
+    this.ngAfterViewInit()
+  }
+
+  ngAfterViewInit() {
+    document.getElementsByClassName('mat-card-header-text')[0].setAttribute('style', 'margin: 0 0');
   }
 
 }
