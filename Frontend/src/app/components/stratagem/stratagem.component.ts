c8a737e075db74e7a9baa2bcc8ab10a6a545ac57
PhilEZad <90150224+PhilEZad@users.noreply.github.com>
2023-02-17 22:00:06 +0100

    Trying to get CSS to behave

--- Frontend/src/app/components/stratagem/stratagem.component.ts
+++ Frontend/src/app/components/stratagem/stratagem.component.ts
@@ -10,50 +10,13 @@ export class StratagemComponent implements OnInit, AfterViewInit {
   @Input() selectedStratagem?: Stratagem;
 
   constructor() {
+
   }
 
   ngOnInit(): void {
-    console.log(this.selectedStratagem)
+
   }
 
   ngAfterViewInit() {
-    document.getElementsByClassName('mat-card-header-text')[0].setAttribute('style', 'margin: 0 0');
-  }
-
-  setHeaderColor(color: string): void {
-    let header = document.getElementById("testid");
-
-    if (header != null) {
-      switch (color) {
-        case 'tactic':
-        {
-          header.style.backgroundColor = '#304c64';
-          break;
-        }
-        case 'ploy': {
-          header.style.backgroundColor = '#246656';
-          break;
-        }
-        case 'epic': {
-          header.style.backgroundColor = '#683c2c';
-          break;
-        }
-        case 'requisition':
-        {
-          header.style.backgroundColor = '#c0241c';
-          break;
-        }
-        case 'wargear':
-        {
-          header.style.backgroundColor = '#60686a';
-          break;
-        }
-        default:
-        {
-          header.style.backgroundColor = 'yellow'
-          break;
-        }
-      }
-    }
   }
 }
